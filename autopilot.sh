013df388fd9aef28bbdd577d5d5e0c9bd14da3b6
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2022-11-05 10:02:04 -0400

    Add files via upload

--- autopilot.sh
+++ autopilot.sh
@@ -24,10 +24,10 @@ then
 else
     echo -e ": \033[1;31m Node.js is not installed\033[0m"
     echo -e "\033[0;32m[+] Installing Node.js\033[0m | source: https://deb.nodesource.com/setup_19.x"
-    curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt-get install -y nodejs 
+    curl -fsSL "https://deb.nodesource.com/setup_19.x" | sudo -E bash - && sudo apt-get install -y nodejs
 fi
 
-#---handle node & npm---
+#---handle yarn---
 echo -ne "\n[*] Checking Yarn"
 
 if command -v yarn &> /dev/null
@@ -47,7 +47,6 @@ then
 else
     echo -e ": \033[1;31m Neovim is not installed\033[0m" 
     echo -e "\033[0;32m[+] Installing nvim\033[0m"
-
     sudo apt-get install neovim 
 fi
 
@@ -60,7 +59,8 @@ do
 
     if [ "$USER_INPUT" = "y" ] || [ "$USER_INPUT" = "Y" ]
     then
-        sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/3270.zip && unzip ./3270.zip -d ~/.fonts && fc-cache -fv && sudo rm -r ./3270.zip 
+        [[ ! -f "./3270.zip" ]] && sudo wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/3270.zip"
+        unzip "./3270.zip" -d "$HOME/.fonts" && fc-cache -fv 
         break
     elif [ "$USER_INPUT" = "n" ] || [ "$USER_INPUT" = "N" ]
     then
@@ -81,11 +81,12 @@ if [[ ! -d "$HOME/.local/share/nvim" ]]
 then
     echo -e "\033[0;32m[+] Installing Vim-Plug\033[0m | source: https://github.com/junegunn/vim-plug"
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' || exit 1
 else
     echo -e ":\033[0;32m Vim-Plug is installed\033[0m"
 fi
 
 #---handle init.vim---
-echo -e "\n\033[0;32m[+] Cloning https://github.com/GabrielFlores8227/nvim\033[0m"
-[[ ! -d "$HOME/.config/nvim" ]] && git clone "https://github.com/GabrielFlores8227/nvim" && mv "./nvim" "$HOME/.config/nvim" && echo -e "\n\033[1;32m🔥 All setup, execute :PlugInstall and enjoy coding! 🔥\033[m" || echo -e "\033[0;31m[x] nvim folder already exists ($HOME/.config/nvim), remove existing nvim folder and try again\033[m" && exit 1
+[[ ! -d "$HOME/.config" ]] mkdir "$HOME/.config"
+[[ ! -d "$HOME/.config/nvim" ]] mkdir "$HOME/.config/nvim"
+[[ ! -f "$HOME/.config/nvim/init.vim" ]] && mv "./init.vim" "$HOME/.config/nvim" && echo -e "\033[0;32m[+] All setup, execute :PlugInstall and enjoy coding!\033[0m" || echo -e "\033[0;31m[x] init.vim already exist ("$HOME/.config/nvim/init.vim"); remove or rename init.vim and try again\033[0m"
