ec1d8c4f27010a148665d762963dd2513602b9f6
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-05-07 20:46:22 -0300

    Update and rename linux-driver.sh to unix-driver.sh

--- unix-driver.sh
+++ unix-driver.sh
@@ -1,69 +1,6 @@
 #!/bin/bash
 
-# Detect the package manager
-if command -v apt >/dev/null; then
-  package_manager="apt"
-elif command -v zypper >/dev/null; then
-  package_manager="zypper"
-elif command -v yum >/dev/null; then
-  package_manager="yum"
-elif command -v dnf >/dev/null; then
-  package_manager="dnf"
-elif command -v pacman >/dev/null; then
-  package_manager="pacman"
-else
-  echo "Package manager not found"
-  exit 1
-fi
-
-# Install Python3
-function python3Driver() {
-  if [[ "$package_manager" == "apt" ]]; then
-    sudo apt install -y python3 python3-pip
-  elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
-    sudo $package_manager install -y python3 python3-pip
-  elif [[ "$package_manager" == "zypper" ]]; then
-    sudo zypper -n install python3 python3-pip
-  elif [[ "$package_manager" == "pacman" ]]; then
-    sudo pacman -S --noconfirm python python-pip
-  fi
-
-  # Install Neovim Python package
-  sudo python3 -m pip install neovim
-}
-
-# Install Yarn
-function yarnDriver() {
-    if [[ "$package_manager" == "apt" ]]; then
-      sudo apt-get install -y yarn
-    elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
-      curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo && sudo $package_manager install yarn
-    elif [[ "$package_manager" == "zypper" ]]; then
-      sudo zypper ar -f https://download.opensuse.org/repositories/devel:languages:nodejs/openSUSE_Tumbleweed/ nodejs
-      sudo zypper refresh
-      sudo zypper -n install yarn
-    elif [[ "$package_manager" == "pacman" ]]; then
-      sudo pacman -S --noconfirm yarn
-    fi
-}
-
-# Install Node.js
-function nodejsDriver() {
-  if [[ "$package_manager" == "apt" ]]; then
-    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-  elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
-    curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-    sudo $package_manager install -y nodejs
-  elif [[ "$package_manager" == "zypper" ]]; then
-    sudo zypper -n install nodejs16
-  elif [[ "$package_manager" == "pacman" ]]; then
-    sudo pacman -S --noconfirm nodejs npm
-  fi
-}
-
-#!/bin/bash
-
+#---Functions----------------------------------------------------------------------------------------
 # Detect the package manager
 if command -v zypper >/dev/null; then
   package_manager="zypper"
@@ -76,10 +13,53 @@ elif command -v dnf >/dev/null; then
 elif command -v pacman >/dev/null; then
   package_manager="pacman"
 else
-  echo "Package manager not found"
-  echo -e "\n\033[0;37;41m[x] Package manager not found | PRESS ENTER TO EXIT\033[0m"
+  echo -e "\n\033[0;37;41m[x] Package manager not found\033[0m"
   exit 1
 fi
+
+# Install Python3
+function python3Driver() {
+  if [[ "$package_manager" == "apt" ]]; then
+    sudo apt install -y python3 python3-pip
+  elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
+    sudo $package_manager install -y python3 python3-pip
+  elif [[ "$package_manager" == "pacman" ]]; then
+    sudo pacman -S --noconfirm python python-pip
+  elif [[ "$package_manager" == "brew" ]]; then
+    brew install python3
+  fi
+
+  # Install Neovim Python package
+  sudo python3 -m pip install neovim
+}
+
+# Install Node.js
+function nodejsDriver() {
+  if [[ "$package_manager" == "apt" ]]; then
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+  elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
+    curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+    sudo $package_manager install -y nodejs
+  elif [[ "$package_manager" == "pacman" ]]; then
+    sudo pacman -S --noconfirm nodejs npm
+  elif [[ "$package_manager" == "brew" ]]; then
+    brew install node
+  fi
+}
+
+# Install Yarn
+function yarnDriver() {
+    if [[ "$package_manager" == "apt" ]]; then
+      sudo apt-get install -y yarn
+    elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
+      curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo && sudo $package_manager install yarn
+    elif [[ "$package_manager" == "pacman" ]]; then
+      sudo pacman -S --noconfirm yarn
+    elif [[ "$package_manager" == "brew" ]]; then
+      brew install yarn
+    fi
+}
 
 # Install Neovim
 function neovimDriver() {
@@ -88,10 +68,10 @@ function neovimDriver() {
     sudo apt install -y neovim python3-neovim
   elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
     sudo $package_manager install -y neovim python3-neovim
-  elif [[ "$package_manager" == "zypper" ]]; then
-    sudo zypper install -y neovim python3-neovim
   elif [[ "$package_manager" == "pacman" ]]; then
     sudo pacman -S --noconfirm neovim python-neovim
+  elif [[ "$package_manager" == "brew" ]]; then
+    brew install neovim
   fi
 }
 
@@ -101,41 +81,27 @@ function unzipDriver() {
     sudo apt-get install -y unzip
   elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
     sudo $package_manager install -y unzip
-  elif [[ "$package_manager" == "zypper" ]]; then
-    sudo zypper install -y unzip
   elif [[ "$package_manager" == "pacman" ]]; then
     sudo pacman -S --noconfirm unzip
+  elif [[ "$package_manager" == "brew" ]]; then
+    brew install unzip
   fi
 }
 
 # Install nerd-fonts
 function nerdFontsDriver() {
     TEMP=$(mktemp)
-    wget -O $TEMP https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/3270.zip && unzip $TEMP -d $HOME/.fonts
+    if [[ "$package_manager" == "brew" ]]; then
+        curl -L -o $TEMP https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/3270.zip && unzip $TEMP -d $HOME/Library/Fonts/
+    elif [[ "$(uname)" == "Linux" ]]; then
+        wget -O $TEMP https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/3270.zip && unzip $TEMP -d $HOME/.fonts
+    fi
 }
 
 # Install vim-plug
 function vimPlugDriver() {
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-}
-
-# Install git 
-function gitDriver() {
-  if [[ "$package_manager" == "apt" ]]; then
-    sudo apt-get install -y git
-  elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
-    sudo $package_manager install -y git
-  elif [[ "$package_manager" == "zypper" ]]; then
-    sudo zypper install -y git
-  elif [[ "$package_manager" == "pacman" ]]; then
-    sudo pacman -S --noconfirm git
-  fi
-}
-
-# Install nvim-plugs
-function nvimPlugsDriver() {
-    git clone https://github.com/GabrielFlores8227/nvim $HOME/.config/nvim && rm -r $HOME/.config/nvim/assets $HOME/.config/nvim/README.md $HOME/.config/nvim/*.sh 
 }
 
 function executeDriver() {
@@ -145,8 +111,23 @@ function executeDriver() {
       || echo -e "\n\033[0;37;41m[x] $1 could not be installed | PRESS ENTER TO CONTINUE\033[0m" && read -sp ""
 }
 
+function gitDriver() {
+  if [[ "$package_manager" == "apt" ]]; then
+    sudo apt-get install -y git
+  elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
+    sudo $package_manager install -y git
+  elif [[ "$package_manager" == "pacman" ]]; then
+    sudo pacman -S --noconfirm git
+  elif [[ "$package_manager" == "brew" ]]; then
+    brew install git
+  fi
+}
 
-# Main
+function nvimPlugsDriver() {
+    git clone https://github.com/GabrielFlores8227/nvim $HOME/.config/nvim && rm -r $HOME/.config/nvim/assets $HOME/.config/nvim/README.md $HOME/.config/nvim/*.sh 
+}
+
+#---Main---------------------------------------------------------------------------------------------
 if [[ "$package_manager" == "zypper" ]]; then
     sudo zypper update
 elif [[ "$package_manager" == "apt" ]]; then
@@ -155,6 +136,8 @@ elif [[ "$package_manager" == "yum" || "$package_manager" == "dnf" ]]; then
     sudo yum update
 elif [[ "$package_manager" == "pacman" ]]; then
     sudo pacman -Sy
+elif [[ "$package_manager" == "brew" ]]; then
+    brew update
 fi
 
 executeDriver "python" "python3Driver"
