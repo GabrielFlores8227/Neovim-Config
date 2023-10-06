4226a362fc1dddd418c6a6d111a36b4215a8be63
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-10-06 15:16:11 -0300

    Update linux-driver.sh

--- linux-driver.sh
+++ linux-driver.sh
@@ -73,6 +73,9 @@ fi
 # python
 download_and_install "https://github.com/GabrielFlores8227/Linux-Packages/blob/main/latest-python/latest-python.sh"
 
+# python-pip
+download_and_install "https://github.com/GabrielFlores8227/Linux-Packages/blob/main/latest-pip/latest-pip.sh"
+
 # curl
 download_and_install "https://github.com/GabrielFlores8227/Linux-Packages/blob/main/latest-curl/latest-curl.sh"
 
